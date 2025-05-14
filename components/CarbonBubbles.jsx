5c546d0af9b5844d2cc9c88607ecb2efaa717c8c
Shoishob007 <u1704049@student.cuet.ac.bd>
2025-05-14 22:34:05 +0600

    some minor changes

--- components/CarbonBubbles.jsx
+++ components/CarbonBubbles.jsx
@@ -14,9 +14,9 @@ export default function CarbonBubbles() {
   return (
     <div className="fixed inset-0 overflow-hidden pointer-events-none">
       <div className="absolute w-[250px] h-[250px] bg-green-300 rounded-full opacity-50 top-[15%] left-[5%] animate-float-1" />
-      <div className="absolute w-[150px] h-[150px] bg-teal-500 rounded-full opacity-50 bottom-[15%] right-[10%] animate-float-2" />
-      <div className="absolute w-[200px] h-[200px] bg-lime-300 rounded-full opacity-50 top-[60%] left-[25%] animate-float-3" />
-      <div className="absolute w-[150px] h-[150px] bg-cyan-400 rounded-full opacity-50 top-[10%] right-[18%] animate-float-4" />
+      <div className="absolute w-[180px] h-[180px] bg-teal-500 rounded-full opacity-50 bottom-[15%] right-[16%] animate-float-2" />
+      <div className="absolute w-[200px] h-[200px] bg-lime-300 rounded-full opacity-50 top-[70%] left-[25%] animate-float-3" />
+      <div className="absolute w-[140px] h-[140px] bg-cyan-400 rounded-full opacity-50 top-[10%] right-[10%] animate-float-4" />
     </div>
   );
 }
